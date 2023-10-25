9a1b406a3dc21f42f247b753ffab710b460d45d6
ariasmariano <marianoariasmdp@gmail.com>
2023-10-25 07:53:46 +0200

    conexion API clima

---
+++ JS/clima.js
@@ -0,0 +1,44 @@
+let lon;
+let lat;
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let loc = document.querySelector(".location");
+let icon =document.querySelector(".icon");
+const kelvin = 273.15;
+
+
+
+window.addEventListener("load", () => {
+
+if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        console.log(position);
+        lon = position.coords.longitude;
+        lat = position.coords.latitude;
+
+        //API
+    const api = "70976a1f7178e791b468aa213a29593f";
+
+
+    const url_base = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${api}`
+    // const url_base = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon-${lon}&appid=${api}`;
+
+        fetch(url_base)
+        .then((response) => {
+            console.log("RESPUESTA JSON")
+            return response.json();
+        })
+
+        .then((data)  => {
+            console.log("ESTA ES LA DATA")
+            console.log(data);
+
+            temperature.textContent =
+                Math.floor(data.list[0].main.temp - kelvin) + "°C";
+            summary.textContent = data.list[0].weather[0].description;
+            loc.textContent = data.city.name + ", " + data.city.country;
+        })
+    })
+}})
